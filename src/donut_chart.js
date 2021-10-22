40ef4c9172f10da9a4425c05e1e2827f7cb7afea
olschowsky <olschowsky@image-systems.biz>
2021-10-22 14:36:33 +0200

    fixing wrong slices and labels color

--- src/donut_chart.js
+++ src/donut_chart.js
@@ -4,26 +4,33 @@ import {accumulate_amount} from "./helper.js";
 
 // setup
 const ZOOM_FACTOR = 0.5;
-const KEY_ID_NUMBER = "anzahl";
-const KEY_ID_NAME = "vorname";
 
+let Keys = null;
+let Chart = null;
+
+
+const createObject = function (...key_value_pairs) {
+    const obj = {};
+    if (key_value_pairs instanceof Array && key_value_pairs.length % 2 === 0) {
+        for (let i = 0; i < key_value_pairs.length; i = i + 2) {
+            obj[key_value_pairs[i]] = key_value_pairs[i + 1];
+        }
+    }
+    return obj;
+};
 
 /** @type {Array<string>} */
 let legacy_names = [];
 const LEGACY_TRESHOLD = 10;
-const prepare_dataset_by_index = function (database, index, number_of_places) {
+const prepare_dataset_by_index = function (current_dataset, key, maximum, number_of_places) {
 
-    const current_key = Object.keys(database)[index];
-
-    /** @type {Array} */
-    let current_dataset = database[current_key].names;
-    if (current_dataset[0].hasOwnProperty("position")) {
-        current_dataset = current_dataset.filter(name => name.position === 1);
+    if (current_dataset[0].hasOwnProperty(Keys.range.key)) {
+        current_dataset = current_dataset.filter(set => set[Keys.range.key] <= Keys.range.max);
     }
 
     const data_snippet = [];
     legacy_names.forEach(name => {
-        const foundItem = current_dataset.find(elem => elem[KEY_ID_NAME] === name && elem[KEY_ID_NUMBER] >= LEGACY_TRESHOLD);
+        const foundItem = current_dataset.find(elem => elem[Keys.id.key] === name && elem[Keys.quantity.key] >= LEGACY_TRESHOLD);
         if (foundItem) {
             data_snippet.push(foundItem);
         }
@@ -37,24 +44,25 @@ const prepare_dataset_by_index = function (database, index, number_of_places) {
     }
     for (let i = 0; i < number_of_places; i = i + 1) {
         const temp_data = current_dataset[i];
-        const temp_name = temp_data[KEY_ID_NAME];
+        const temp_name = temp_data[Keys.id.key];
         if (!legacy_names.includes(temp_name)) {
             data_snippet.push(temp_data);
         }
     }
-    legacy_names = data_snippet.map(elem => elem[KEY_ID_NAME]);
+    legacy_names = data_snippet.map(elem => elem[Keys.id.key]);
 
     console.log("legacy: " + JSON.stringify(legacy_names));
     console.log("snippet: " + JSON.stringify(data_snippet));
 
-    const size = database[current_key].amount;
-    const remaining = size - accumulate_amount(data_snippet, KEY_ID_NUMBER);
-    data_snippet.push({vorname: "sonstige", anzahl: remaining});
+    // TODO replace "sonstige" with variable
+    const remaining = maximum - accumulate_amount(data_snippet, Keys.quantity.key);
+    data_snippet.push(createObject(Keys.id.key, "sonstige", Keys.quantity.key, remaining));
 
-    const unique_names = current_dataset.filter(name => name[KEY_ID_NUMBER] === 1);
-    data_snippet.push({vorname: "einizgartig", anzahl: unique_names.length});
+    // TODO replace "einzigartig" with variable
+    const unique_names = current_dataset.filter(set => set[Keys.quantity.key] === 1);
+    data_snippet.push(createObject(Keys.id.key, "einzigartig", Keys.quantity.key, unique_names.length));
 
-    return {data: data_snippet, size, year: current_key};
+    return {data: data_snippet, size: maximum, title: key};
 };
 
 const calculate_side = (number) => (number * ZOOM_FACTOR) / Math.sqrt(2);
@@ -91,7 +99,9 @@ const setup_donut_chart = function (diagonal) {
 
 const calculate_mid_angle = arc => arc.startAngle + (arc.endAngle - arc.startAngle) / 2;
 
-const update_donut_chart = function (chart, dataset) {
+const color_scale = d3.scaleOrdinal(d3.schemeTableau10);
+
+const update_donut_chart = function (dataset) {
 
     const prepared_data = dataset.data;
     console.log(JSON.stringify(prepared_data));
@@ -100,16 +110,11 @@ const update_donut_chart = function (chart, dataset) {
     const margin = 50;
     const radius = side / 2 - margin;
 
-    const color_domain = ["sonstige", "einzigartig", ...legacy_names];
-    const color_scale = d3.scaleOrdinal()
-        .domain(color_domain)
-    // TODO try to reduce scheme/range to size of domain
-        .range(d3.schemeSet2);
-    console.log(color_scale.domain(), color_scale.range());
-    console.log(color_scale("einzigartig"), color_scale("einzigartig"), color_scale("einzigartig"), color_scale("einzigartig"));
+    // TODO replace strings with integer
+    color_scale.domain(["sonstige", "einzigartig", ...legacy_names]);
 
     const data_as_arcs = d3.pie()
-        .value(d => d[KEY_ID_NUMBER])
+        .value(d => d[Keys.quantity.key])
         (prepared_data);
 
     const inner_arc = d3.arc()
@@ -120,7 +125,7 @@ const update_donut_chart = function (chart, dataset) {
         .innerRadius(radius * 0.9)
         .outerRadius(radius * 0.9);
 
-    const slices = chart.select(".Slices").selectAll("path.Slice")
+    const slices = Chart.select(".Slices").selectAll("path.Slice")
         .data(data_as_arcs);
     // console.log(color_scale("Sophie"));
     slices
@@ -128,72 +133,87 @@ const update_donut_chart = function (chart, dataset) {
         .append("path")
         .attr("class", "Slice")
         .merge(slices)
-        .style("fill", d => {console.log(d.data[KEY_ID_NAME], color_scale(d.data[KEY_ID_NAME])); return color_scale(d.data[KEY_ID_NAME]);})
+        .style("fill", d => color_scale(d.data[Keys.id.key]))
         .transition().duration(1000)
         .attr("d", inner_arc);
 
-    const lines = chart.select(".Lines").selectAll("polyline")
-        .data(data_as_arcs);
+    // const lines = Chart.select(".Lines").selectAll("polyline")
+    //     .data(data_as_arcs);
     
-    lines.enter()
-        .append("polyline")
-        .merge(lines)
-        .transition().duration(1000)
-            .attr('points', d => [inner_arc.centroid(d), label_arc.centroid(d)]);
+    // lines.enter()
+    //     .append("polyline")
+    //     .merge(lines)
+    //     .transition().duration(1000)
+    //         .attr('points', d => [inner_arc.centroid(d), label_arc.centroid(d)]);
     
-    const labels = chart.select(".Labels").selectAll("text")
-        .data(data_as_arcs);
+    // const labels = Chart.select(".Labels").selectAll("text")
+    //     .data(data_as_arcs);
 
-    labels.enter()
-        .append("text")
-        .merge(labels)
-        .attr("dy", ".3em") // HARD CODED
-        .transition().duration(1000)
-        .text(d => d.data[KEY_ID_NAME])
-            .attr('transform', d => `translate(${label_arc.centroid(d)})`)
-            .style('text-anchor', d => calculate_mid_angle(d) < Math.PI ? "start" : "end");
+    // labels.enter()
+    //     .append("text")
+    //     .merge(labels)
+    //     .attr("dy", ".3em") // HARD CODED
+    //     .transition().duration(1000)
+    //     .text(d => d.data[Keys.id.key])
+    //         .attr('transform', d => `translate(${label_arc.centroid(d)})`)
+    //         .style('text-anchor', d => calculate_mid_angle(d) < Math.PI ? "start" : "end");
 
-    const legend = chart.select(".Legend");
+    const legend = Chart.select(".Legend");
 
     legend.selectAll("circle")
-        .data(color_domain)
+        .data(color_scale.domain())
         .enter()
         .append("circle")
             .attr("cx", 0)
-            .attr("cy", function(d,i){ return i * 25})
+            .attr("cy", (d, i) => i * 25)
             .attr("r", 7)
-            .style("fill", d => {console.log(d, color_scale(d)); return color_scale(d);});
+            .style("fill", d => color_scale(d));
     // console.log(color_scale("Sophie"));
     legend.selectAll("text")
-        .data(color_domain)
+        .data((color_scale.domain()))
         .enter()
         .append("text")
             .attr("x", 20)
-            .attr("y", function(d,i){ return 5 + i * 25})
-            .style("fill", d => {console.log(d, color_scale(d)); return color_scale(d);})
+            .attr("y", (d, i) => 5 + i * 25)
+            .style("fill", d => color_scale(d))
             .text(d => d)
             .attr("text-anchor", "left")
             .style("alignment-baseline", "middle");
 
-    chart.select(".Year")
-        .text(dataset.year);
+    Chart.select(".Year")
+        .text(dataset.title);
 };
 
-const visualize = function (database, maximum) {
+const get_all_value_maxima = function (datasource, quantity_key) {
+    const maxima = [];
+    for (const key in datasource) {
+        maxima.push(accumulate_amount(datasource[key], quantity_key));
+    }
+    return maxima;
+};
 
+const visualize = function (datasource, encoding) {
 
-    const length = Object.values(database).length;
+    // TODO check encoding correctness
+    Keys = encoding;
 
-    const donut_chart = setup_donut_chart(maximum);
+    const maxima = get_all_value_maxima(datasource, Keys.quantity.key);
+    Chart = setup_donut_chart(Math.max(...maxima));
 
+    const length = Object.values(datasource).length;
     let i = 0;
     const chart_animation_timer = d3.timer((elapsed) => {
 
         if (elapsed > 2000 * i) {
 
-            const dataset = prepare_dataset_by_index(database, i, 3);
+            const current_key = Object.keys(datasource)[i];
+            update_donut_chart(prepare_dataset_by_index(
+                datasource[current_key],
+                current_key,
+                maxima[i],
+                3
+            ));
 
-            update_donut_chart(donut_chart, dataset);
             i = i + 1;
             
             // chart_animation_timer.stop();
@@ -208,4 +228,4 @@ const visualize = function (database, maximum) {
 };
 
 
-export {accumulate_amount, visualize};
+export {visualize};
--- src/index.js
+++ src/index.js
@@ -6,32 +6,22 @@ import * as DonutChart from "./donut_chart.js";
 import * as LineChart from "./line_chart.js";
 
 
-const KEY_ID_NUMBER = "anzahl";
-const KEY_ID_NAME = "vorname";
-
-const basic_data_preparation = function (data) {
-
-};
-
 
 get_data_from_backup().then(loaded_data => {
 
-    const db_girls_born_by_year = {};
-
     // prepare fetched data for donut chart
     // TODO move to donut_chart.js ??
-    let amount_of_most_girls_born = -1;
-    for (const key in loaded_data) {
-        db_girls_born_by_year[key] = {};
-        db_girls_born_by_year[key].names = loaded_data[key];
-
-        const amount = accumulate_amount(loaded_data[key], KEY_ID_NUMBER);
-        db_girls_born_by_year[key].amount = amount
-        if (amount > amount_of_most_girls_born) {
-            amount_of_most_girls_born = amount;
+    
+    // TODO specify encoding
+    DonutChart.visualize(
+        loaded_data,
+        {
+            id: {key: "vorname", type: "nominal"},
+            quantity: {key: "anzahl", type: "quantity"},
+            range: {key: "position", type: "index", max: 1}
         }
-    }
-    DonutChart.visualize(db_girls_born_by_year, amount_of_most_girls_born);
+    );
+    // (basic_data_preparation(loaded_data));
 
     // prepare data for line chart
     // const names_variety = [];
